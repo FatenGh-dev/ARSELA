df11e8a89c3e5259879197286c538cafdee755a3
faten gharbi <52913763+FatenGh-dev@users.noreply.github.com>
2020-12-26 08:58:05 +0100

    Add files via upload

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,1 +1,28 @@
+const express=require('express')
+const router=express.Router()
 
+const signUpTemplateCopy=require('../models/SignUpModels')
+
+const bcrypt = require('bcrypt')
+router.post('/signup/', async(request,response) => {
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword=await 
+    bcrypt.hashSync(request.body.password,saltPassword)
+    const SignedupUser =  new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        username:request.body.username,
+        email:request.body.email,
+        password:securePassword
+    })
+    
+    SignedupUser.save()
+    .then(data => {
+        response.json(data)
+    })
+    .catch(error => {
+            response.json(error)
+    }) })
+  
+      
+module.exports = router
